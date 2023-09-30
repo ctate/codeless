061d833de9a796e77aec1a0c5e3d09e484b58a26
Chris Tate <chris@ctate.dev>
2023-09-30 10:08:30 -0500

    uses 16k model

--- app/api/chat/edge/route.ts
+++ app/api/chat/edge/route.ts
@@ -95,7 +95,7 @@ export async function POST(req: NextRequest) {
   ]
 
   const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-3.5-turbo-16k',
     stream: true,
     messages: systemMessages.concat(
       messages.map((message) => ({
