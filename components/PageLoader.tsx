c47bf9ca3e4e89d2fbcb18095daef10af2c61f31
Chris Tate <chris@ctate.dev>
2024-11-07 00:27:11 -0600

    removes code 2

--- components/PageLoader.tsx
+++
@@ -1,8 +0,0 @@
-import { CircularProgress, Stack } from '@mui/material'
-import { FC } from 'react'
-
-export const PageLoader: FC = () => (
-  <Stack alignItems="center" height="100vh" justifyContent="center">
-    <CircularProgress sx={{ color: 'white' }} />
-  </Stack>
-)
--- utils/loadData.ts
+++
@@ -1,8 +0,0 @@
-import { existsSync } from 'fs'
-import { readFile } from 'fs/promises'
-
-export const loadData = async <T>(file: string) => {
-  const data = existsSync(file) ? JSON.parse(await readFile(file, 'utf8')) : {}
-
-  return data as T
-}
