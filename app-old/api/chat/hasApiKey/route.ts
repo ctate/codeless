95102f659277d4a09096fe50f044d840346ad024
Chris Tate <chris@ctate.dev>
2024-11-07 00:25:02 -0600

    archive 2

--- app-old/api/chat/hasApiKey/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server'
-
-export async function POST(req: NextRequest) {
-  return NextResponse.json({
-    hasApiKey: !!process.env.OPENAI_API_KEY
-  })
-}
--- app-old/api/mode/getMode/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server'
-
-export async function POST(req: NextRequest) {
-  return NextResponse.json({
-    mode: process.env.MODE,
-  })
-}
